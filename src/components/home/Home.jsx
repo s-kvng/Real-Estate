860c2e7dede6539fb0ba7b9a5d93298dd2b2fadc
nathan <kvngnathan8420@gmail.com>
2023-05-30 17:46:26 +0000

    Add featured section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Hero/Hero';
+import Featured from '../featured/Featured';
 
 const Home = () => {
   return (
     <>
         <Hero />
+        <Featured />
     </>
   )
 }
