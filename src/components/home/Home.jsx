ca70bf4778b39f60ff0ea8f31cfc02cb07ac40e9
nathan <kvngnathan8420@gmail.com>
2023-05-31 02:02:01 +0000

    Add Award section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './Hero/Hero';
 import Featured from '../featured/Featured';
 import Recent from './recent/Recent';
+import Award from './awards/Award';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Hero />
         <Featured />
         <Recent />
+        <Award />
     </>
   )
 }
